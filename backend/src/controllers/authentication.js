112f7292b7d00a138f2a32203bf2fafd17b4e8c3
juanrealpe972 <juanrealpe972@gmail.com>
2024-02-29 15:37:43 -0500

    new hello

--- backend/src/controllers/authentication.js
+++ backend/src/controllers/authentication.js
@@ -21,16 +21,25 @@ export const iniciarUser = async (req, res) => {
   }
 };
 
-export const verificarUsuario = async (req, res) => {
+export const verificarUsuario = async (req, res, next) => {
   const [rows] = await pool.query(`SELECT * FROM usuarios`);
   try {
-    const token_client = req.headers("token");
+    const token_client = req.header("token");
     if (!token_client)
       return res.status(404).json({ message: "token es requerido" });
-    jwt.verify(token, process.env.AUT_SECRET, async (err) => {
-      if (err) return res.status(401).json({ message: "token no autorizado" });
-      res.status(200).json({ message: "El usuario es", data: rows });
-    });
+    else {
+      const token = jwt.verify(
+        token_client,
+        process.env.AUT_SECRET,
+        async (error, decoded) => {
+          if (error)
+            return res.status(401).json({ message: "token no valido" });
+          else {
+            next();
+          }
+        }
+      );
+    }
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
